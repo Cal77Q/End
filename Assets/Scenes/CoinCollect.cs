26d96654c9795d291c3693a623051d0f1358ddcf
Cal77Q <cqi2024@elam.co.uk>
2024-10-07 15:39:29 +0100

    hjrj

---
+++ Assets/Scenes/CoinCollect.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+   
+
+        void OntriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("player"))
+            {
+                Destroy(gameObject);
+
+            }
+        }
+
+    
+
+
+}
+ 
